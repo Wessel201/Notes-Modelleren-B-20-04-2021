fafdc3125df8feb893c50e2e069cdabac3fd7170
Wessel <wessel.van.sommeren@gmail.com>
2021-04-29 12:04:04 +0200

    Cost

--- cost_functie.py
+++ cost_functie.py
@@ -1,11 +1,47 @@
 import pandas as pd 
 
-xls = pd.ExcelFile('large.xlsx')
-df1 = pd.read_excel(xls, 'w')
-df2 = pd.read_excel(xls, 'c')
-df3 = pd.read_excel(xls, 'f')
+
+file_name ='Large.xlsx'
+class parcel():
+	def __init__(self,file):
+		xls = pd.ExcelFile(file)
+		self.flow = pd.read_excel(xls, 'w')
+		self.package_cost = pd.read_excel(xls, 'c')
+		self.hub_cost = pd.read_excel(xls, 'f')
+		self.cities = len(self.flow.columns)
+
+	def calculate_cost(self,hubs):
+		cost = 0
+		index = {}
+		flow_copy = self.flow
+		cost_copy = self.package_cost
+		cost_copy = cost_copy.take([hub - 1 for hub in hubs])
+		print(cost_copy)
+		for i in range(1,self.cities):
+			if i in hubs:
+				index[i] = i
+				continue
+			nearest_hub = cost_copy.loc[cost_copy[i] > 0 , i].idxmin()
+			cost += cost_copy[i][nearest_hub] * flow_copy[i].sum()
+			nearest_hub = nearest_hub + 1
+			print(i,nearest_hub)
+			index[i] = nearest_hub
+			flow_copy[nearest_hub] = flow_copy[i]+flow_copy[nearest_hub]
+			flow_copy = flow_copy.drop(columns = i)
+		print(index)
+		# for i in range(self.cities):
+		# 	to_hub = index[i]
+		# 	flow_copy.iloc[i]
+
+		print(flow_copy.iloc[14])
 
 
-print(df1)
-print(df2)
-print(df3)
+
+
+test = parcel(file_name) 
+test.calculate_cost([1,2,3,9])
+
+
+print(test.package_cost.loc[test.package_cost[14] > 0, 14].idxmin())
+print(test.package_cost[14])
+print(test.package_cost)
