efa3748b482a0bf5a0ca4f706af2220436326ed5
Wessel <wessel.van.sommeren@gmail.com>
2021-05-04 14:49:08 +0200

    hij werkt

--- cost_functie.py
+++ cost_functie.py
@@ -9,7 +9,7 @@ class parcel():
 		xls = pd.ExcelFile(file)
 		self.flow = pd.read_excel(xls, 'w')
 		self.package_cost = pd.read_excel(xls, 'c')
-		self.hub_cost = pd.read_excel(xls, 'f')
+		self.hub_cost =[13530] + list(pd.read_excel(xls, 'f')[13530])
 		self.cities = len(self.flow.columns)
 
 	def calculate_cost(self,hubs):
@@ -19,26 +19,32 @@ class parcel():
 		cost_copy = self.package_cost
 		cost_copy = cost_copy.take([hub - 1 for hub in hubs])
 		cost_copy = cost_copy.drop(columns= 'Unnamed: 0')
-		print(cost_copy)
 		for i in range(1,self.cities):
 			if i in hubs:
 				index[i] = i
+				cost+= self.hub_cost[i-1]
 				continue
 			nearest_hub = cost_copy.loc[cost_copy[i] > 0 , i].idxmin()
-			cost += cost_copy[i][nearest_hub] * flow_copy[i].sum()
+			cost += cost_copy[i][nearest_hub] * flow_copy[i].sum()*collection_factor
 			nearest_hub = nearest_hub + 1
-			print(i,nearest_hub)
 			index[i] = nearest_hub
 			flow_copy[nearest_hub] = flow_copy[i]+flow_copy[nearest_hub]
 			flow_copy = flow_copy.drop(columns = i)
-		print(index)
 		flow_copy = flow_copy.drop(columns = 'Unnamed: 0')
-		print(self.cities)
 		for i in range(self.cities - 1):
-			print('index = ' + str(i))
-			print(flow_copy.iloc[i])
+			# print('index = ' + str(i))
+			flowss = list(flow_copy.iloc[i])
+			# print(flowss)
 			to_hub = index[i+1]
-			print(cost_copy[to_hub])
+			prices = list(cost_copy[to_hub])
+			# print(prices)
+			total_packages = sum(flowss)
+			transfer_cost = sum([int(flowss[i])*int(prices[i]) for i in range(len(flowss))])
+			distribution_cost = self.package_cost[to_hub][i]*total_packages*distribution_factor
+			# print(distribution_cost)
+			# print(transfer_cost)
+			cost+= distribution_cost + transfer_cost
+		return cost
 
 
 		print(flow_copy)
@@ -47,6 +53,5 @@ class parcel():
 
 
 test = parcel(file_name) 
-test.calculate_cost([1,2,3,9])
-
+print(test.calculate_cost([1,2,3,9]))
 
